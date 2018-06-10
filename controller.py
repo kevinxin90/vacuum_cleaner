537a1ea7749a840fa0dd067a7b3c9c23672322d8
kevinxin90 <kevinxin@scripps.edu>
2018-06-10 11:11:45 -0700

    add move rectangle

--- controller.py
+++ controller.py
@@ -77,7 +77,7 @@ class CarController:
         self.turnRight()
         self.bw.forward()
         self.bw.speed = 40
-        time.sleep(2.1)
+        time.sleep(1.9)
         self.bw.stop()
         self.turnStraight()
         self.bw.forward()
