5d0e3ce854d5b07ff73361573941a628a9c08126
kevinxin90 <kevinxin@scripps.edu>
2018-06-10 07:41:19 -0700

    change runtime

--- controller.py
+++ controller.py
@@ -1,40 +1,89 @@
 from __future__ import print_function
-from time import sleep
-
-###############################################################
-#This part controls the front wheel and backwheel
-###############################################################
 from picar import front_wheels, back_wheels
-
-# bw will handle all the movements for backwheel
-# backwheel controls the speed and direction(moving forward or backward)
-bw = back_wheels.Back_Wheels()
-# fw will handle all the movements for frontwheel
-# front wheel controls the angle while the vehicle turns
-fw = front_wheels.Front_Wheels()
-
-fw._angle = {'straight': 82, 'left': 30, 'right': 130}
+import picar
+from time import sleep
+import cv2
+from pyzbar.pyzbar import decode
 
 
-def single_turn(run_time):
-	fw.turn_straight()
-	sleep(run_time)
-	fw.turn_left()
-	sleep(3.5)
+class CarController:
+    def __init__(self):
+        self.fw = front_wheels.Front_Wheels()
+        self.fw._angle = {'straight': 82, 'left': 32, 'right': 132}
+        self.bw = back_wheels.Back_Wheels()
+        self.speed = 40
+    
+    def moveForward(self, t=0):
+        """
+        move the car forward, then stop
+        """
+        self.turnStraight()
+        time.sleep(0.5)
+        self.bw.forward()
+        self.bw.speed = self.speed
+        # make the car run for t second
+        if t>0:
+            time.sleep(t)
+        self.bw.stop()
+     
+    def moveBackward(self, t=0):
+        """
+        move the car backward, then stop
+        """
+        self.turnStraight()
+        time.sleep(0.5)
+        self.bw.backward()
+        self.bw.speed = self.speed
+        # make the car run for t seconds
+        if t>0: 
+            time.sleep(t)
+        self.bw.stop()
 
-def spiral_move(run_time, turn_num):
-	for i in range(turn_num):
-		single_turn(run_time)
+    def turnLeft(self, angle=0):
+        """
+        turn front wheel to the left
+        """
+        self.fw.turn_left()
+        time.sleep(0.5)
+    
+    def turnRight(self, angle=0):
+        """
+        turn front wheel to the right
+        """
+        self.fw.turn_right()
+        time.sleep(0.5)
+    
+    def turnStraight(self, angle=0):
+        """
+        turn front wheel straight
+        """
+        self.fw.turn_straight()
+        time.sleep(0.5)
 
-def main():
-	bw.backward()
-	bw.speed = 40
-	for i in range(10):
-		print('current front wheel angle parameter: {}'.format(fw._angle))
-		turn_num = 4
-		run_time = i + 1
-		print('current loop number: {}, current run time: {}'.format(i, run_time))
-		spiral_move(run_time, turn_num)
-		print('this loop ends!')
-	bw.stop()
-	fw.turn_straight()
+    def turnLeftNinty(self):
+        """
+        This function is car specific, and also related to friction
+        Be sure to modify the code to fit your car and specific road condition
+        The goal of the code is to make the car position stay the same
+        while changing its "theta" to pi/2 (90degree)
+        """
+        self.fw.turn_straight()
+        self.turnLeft()
+        self.moveForward(4)
+        self.turnRight()
+        self.moveBackward(5.3)
+        self.turnStraight()
+
+    def extract_barcode_location(self) :
+        """
+        This function tries to extract the barcode location
+        information from the image by using pyzbar
+        """
+        # Load input image
+        _, bgr_image = img.read()
+        barcode_info = decode(bgr_image)
+        if barcode_info:
+            # only return the barcode location info
+            return barcode_info[0][2]
+        else:
+            return None
