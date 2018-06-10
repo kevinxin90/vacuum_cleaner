a815ad0f0a8071bb78dc21d17fdcce36c96bcbcc
kevinxin90 <kevinxin@scripps.edu>
2018-06-10 09:15:51 -0700

    change angle

--- controller.py
+++ controller.py
@@ -75,7 +75,7 @@ class CarController:
         self.turnRight()
         self.bw.forward()
         self.bw.speed = 40
-        time.sleep(0.8)
+        time.sleep(1)
         self.turnStraight()
         self.bw.forward()
         self.bw.speed = 40
